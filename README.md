1aa57fe9c876385f6f4cdaa9e9ea61cbeeb46534
alijmo <al.jvdi2@gmail.com>
2022-12-11 14:52:16 +0000

    Realese V1.0.0

---
+++ README.md
@@ -0,0 +1,65 @@
+# DaartAds JavaScript library
+You can use this library to access the DaartAds APIs.
+
+## Requirements
+- [jQuery](https://jquery.com/)
+
+## Installation
+----
+
+#### Using CDN
+```html
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/daart-agency/DaartAds-JS/css/DaartAds.min.css">
+
+
+<!-- jQuery (If you don't have one) -->
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+<!-- JS -->
+<script src="https://cdn.jsdelivr.net/gh/daart-agency/DaartAds-JS/js/DaartAds.min.js" daart-token="API_TOKEN"></script>
+
+<!-- Change The value of daart-token="" with your API_TOKEN -->
+
+```
+
+#### Manual Installation:
+```html
+<!-- CSS -->
+<link rel="stylesheet" href="/path/to/css/daart-ads/min">
+
+<!-- jQuery (If you don't have one) -->
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+<!-- JS -->
+<script src="/path/to/js/daart-ads/min" daart-token="API_TOKEN"></script>
+
+<!-- Change The value of daart-token="" with your API_TOKEN -->
+
+```
+
+- You must initialise the _```daart-token```_ in the script tag, either you choose **CDN** or **manual** use.
+
+## Usage
+
+```html
+<!-- Just copy the code below and paste it wherever you imagine you might need to utilise our API. -->
+
+<daart-ads daart-adsize="CHOSEN_SIZE"></daart-ads>
+```
+- From the table you may see below, you should pick a size for _```daart-adsize```_.
+
+## AdSize
+| SizeID | Width * Height |
+|--------|----------------|
+| 1  | 720 * 480 |
+| 2  | 728 * 90  |
+| 3  | 480 * 320 |
+| 4  | 492 * 328 |
+| 5  | 468 * 60  |
+| 6  | 360 * 240 |
+| 7  | 320 * 100 |
+| 8  | 320 * 50  |
+| 9  | 300 * 250 |
+| 10 | 295 * 98  |
+| 11 | 160 * 600 |
---
+++ js/DaartAds.js
@@ -0,0 +1,113 @@
+/*! DaartAds 1.0.0
+ * ©2022 Alex Javadi, Daart Agency - MIT Licence
+ */
+
+/**
+ * @summary     DaartAgency.DaaertAds
+ * @description Smart Ads jQuery Library by Daart Company
+ * @version     1.0.0
+ * @author      Alex Javadi
+ * @contact     https://daartads.com
+ * @copyright   Daart Agency
+ *
+ * This source file is free software, available under the following license:
+ *   MIT license - https://choosealicense.com/licenses/mit/
+ *
+ * With the hope that it may be helpful, this source file is provided to you WITHOUT ANY WARRANTY, 
+ * not even the implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * Details can be found in the licencing files.
+ */
+
+
+class DaartAds {
+    #_token;
+    #_DAART_API_URL;
+
+    constructor(Token) {
+        this.#_token = Token;
+        this.#_DAART_API_URL = "https://daartads.com/advertising/apiAdv4.php"
+    }
+
+    async GetAds(adSize) {
+        var data = [];
+        await $.ajax({
+            method: "POST",
+            url: this.#_DAART_API_URL,
+            data: {
+                "token" : this.#_token,
+                "adsize": adSize
+            },
+            dataType:"json",
+            crossDomain: true,
+            statusCode: {
+                415: function() {
+                    console.error("The Daart Ads API key is invalid, please double check it.")
+                }
+            }
+        }).done((res) => {
+            $.each(res['data'], (key,val) => {
+                data.push({
+                    "AdDetails": val,
+                    "Callback": this.constructor.GetAdCallBack(val['Cid'],val['Source'],val['adsize'])
+                });
+            })
+        })
+
+        return Promise.resolve(data);
+    }
+
+
+    static GetAdCallBack(Cid,Source,adSize)
+    {
+        return `https://daartads.com/CP.php?Cid=${Cid}&Source=${Source}&adsize=${adSize}`;
+    }
+}
+
+ window.onload = function() {
+    if (window.jQuery) {  
+        (function($) {
+            let token = $("script[daart-token]").attr('daart-token');
+            if(typeof token !== undefined && token.length > 0)
+            {
+                
+                if($("daart-ads").length > 0)
+                {
+                    var api = new DaartAds(token);
+
+                    console.info("Deployment of the Daart Ads library was successful.")
+
+                    $("daart-ads").each(function () {
+                        let adsize = $(this).attr('daart-adsize')
+                        let cr_logo_white = $(this).attr('daart-logo-white') !== undefined;
+                        if(typeof adsize !== undefined && adsize.length > 0)
+                        {
+                            api.GetAds(adsize).then((res) => {
+                                $.each(res, (key,val) => {
+                                    $(this).append(`<a class="da-banner" target="_blank" href="${val['Callback']}"><img src="${val['AdDetails']['image']}" alt="Daartads banner"></a><div class="da-copyright"><a href="https://daartads.com?utm_source=daartads&utm_medium=banner&utm_campaign=logo_click" title="Advertising with Daart Ads"><img src="img/logo/daartads-logo.png" alt="Daartads banner"></a></div>`);
+                                })
+                            })
+                        }
+                        else {
+                            alert("DaartAds critical problem found\ncheck your browser developer console for more information")
+                            console.error("Please verify the tag for one of your daart-ads that lacks a adsize key.")
+                        }
+                    })
+                    
+                    
+                }
+                else
+                {
+                    console.warn("Daart Ads Library was successfully deployed; however, it was never used. \nTo use it, simply copy and paste the following code wherever you wish to display ads. \n<daart-ads daart-adsize='SIZE_YOU_WANT'></daart-ads>")
+                }
+            }
+            else
+            {
+                alert("DaartAds critical problem found\ncheck your browser developer console for more information")
+                console.error("API key for DaartAds Not Found, make sure your API key is entered in the 'daart-token' attr.")
+            }
+        })(jQuery);
+    } else {
+        alert("DaartAds critical problem found\ncheck your browser developer console for more information")
+        console.error("DaartAds will be shut down owing to a major error, error -1, necessary plugins not found. \nPlugin Name: jQuery")
+    }
+}
